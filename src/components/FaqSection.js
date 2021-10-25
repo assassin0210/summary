eda5eeb03d1de58e8f2d6b40b004224958b0eea2
assassin0210 <s0k0lOOFFF>
2021-10-25 21:58:32 +0300

    7 styled FaqSection

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,10 @@
+import {About} from "../style";
+import styled from "styled-components";
 
 
-export const FaqSection =()=>{
-    return(
-        <div className='faq' >
+export const FaqSection = () => {
+    return (
+        <Faq className='faq'>
             <h2>Any question <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do i Start?</h4>
@@ -10,6 +12,7 @@ export const FaqSection =()=>{
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, in.</p>
                 </div>
+                <div className="faq-line"/>
             </div>
             <div className="question">
                 <h4>How Do i Start?</h4>
@@ -17,6 +20,8 @@ export const FaqSection =()=>{
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, in.</p>
                 </div>
+                <div className="faq-line"/>
+
             </div>
             <div className="question">
                 <h4>How Do i Start?</h4>
@@ -24,6 +29,7 @@ export const FaqSection =()=>{
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, in.</p>
                 </div>
+                <div className="faq-line"/>
             </div>
             <div className="question">
                 <h4>How Do i Start?</h4>
@@ -31,8 +37,39 @@ export const FaqSection =()=>{
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, in.</p>
                 </div>
+                <div className="faq-line"/>
             </div>
 
-        </div>
+        </Faq>
     )
 }
+
+const Faq = styled(About)`
+  display: block;
+
+  span {
+    display: block;
+  }
+
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0;
+    cursor: pointer;
+  }
+
+  p {
+    padding: 1rem 0;
+  }
+
+`
