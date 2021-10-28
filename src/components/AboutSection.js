ef59076cd5749d9ba7c8e68e25966ede48ca2d36
assassin0210 <s0k0lOOFFF>
2021-10-28 22:54:09 +0300

    15 add line and animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,8 @@
 import home1 from "../img/home1.png";
 import { About, Description, Hide, Image } from "../style";
 import { motion } from "framer-motion";
+import { fade, photoAnimation, titleAnimation } from "../animation";
+import { Wave } from "./Wave";
 
 export const AboutSection = () => {
   return (
@@ -8,25 +10,26 @@ export const AboutSection = () => {
       <Description className="description">
         <motion.div className="title">
           <Hide className="hide">
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide className="hide">
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               Your
               <span>dreams</span>
               come
             </motion.h2>
           </Hide>
           <Hide className="hide">
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>Contact us for any photography </p>
-        <button>Contact us</button>
+        <motion.p variants={fade}>Contact us for any photography</motion.p>
+        <motion.button variants={fade}>Contact us</motion.button>
       </Description>
       <Image className="image">
-        <img src={home1} alt="" />
+        <motion.img variants={photoAnimation} src={home1} alt="" />
       </Image>
+      <Wave />
     </About>
   );
 };
--- src/style.js
+++ src/style.js
@@ -11,21 +11,24 @@ export const About = styled.div`
 export const Description = styled.div`
   flex: 1;
   padding-right: 5rem;
+  z-index: 2;
 
   h2 {
     font-weight: lighter;
-  }`;
+  }
+`;
 
 export const Image = styled.div`
   flex: 1;
   overflow: hidden;
+  z-index: 2;
 
   img {
     width: 100%;
     height: 80vh;
     object-fit: cover;
   }
-`
+`;
 export const Hide = styled.div`
   overflow: hidden;
-`
+`;
